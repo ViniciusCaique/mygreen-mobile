6f9454964f41de759bfde5154679df408066d4be
Vinicius Caique <viniciuscaiquesilva@gmail.com>
2023-06-07 04:31:36 -0300

    config de ambiente

---
+++ src/screens/Home.js
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+
+export function Home() {
+
+    return(
+        <View style={{ flex: 1, justifyContent: "center" }}>
+            <View  style={{ alignItems: "center" }}>
+                <Text>
+                    teste
+                </Text>
+            </View>
+        </View>
+    )
+}
